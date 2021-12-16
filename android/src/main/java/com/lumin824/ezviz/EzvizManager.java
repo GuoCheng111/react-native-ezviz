dfb0f372a26f38b38e68ab83f2121416b120e6e7
guocheng <guocheng@cyclecentury.com>
2021-12-16 16:30:38 +0800

    for androidX

--- android/src/main/java/com/lumin824/ezviz/EzvizManager.java
+++ android/src/main/java/com/lumin824/ezviz/EzvizManager.java
@@ -1,6 +1,6 @@
 package com.lumin824.ezviz;
 
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReadableArray;
